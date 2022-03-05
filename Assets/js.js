8a4e8118cb7aed3cae8a62d035753ea64013ab51
Alex Wiles <alexandra.wiles6@gmail.com>
2022-03-05 00:12:30 -0500

    Initial HTML setup added, Initial variances added, initial basic css added

---
+++ Assets/js.js
@@ -0,0 +1,50 @@
+var score =0;
+var questions = [
+    { 
+        prompt: "How many Data types are there in Javascript?.",
+        answers: {
+            a: "4",
+            b: "6",
+            c: "8",
+            d: "infinite"
+        },
+         correctAnswer: "c"
+    },
+
+    { 
+        prompt: "What interaction can you do a user input?",
+        answers: {
+            a: "prompt",
+            b: "alert",
+            c: "confirm",
+            d: "you cannot do a user input in any interaction."
+        },
+         correctAnswer: "a"
+    },
+
+    { 
+        prompt: "What would you use to observe a user behavior?",
+        answers: {
+            a: "EventListener",
+            b: ".clickhere",
+            c: "button",
+            d: "event"
+        },
+         correctAnswer: "a"
+    },
+
+    { 
+        prompt: "What do you use to call a function?",
+        answers: {
+            a: "Square Brackets",
+            b: "Quotes",
+            c: "Curly Brace",
+            d: "Parenthesis"
+        },
+         correctAnswer: "d"
+    },
+]
+
+for (i=0, i < questions.length, i++) {
+
+}
