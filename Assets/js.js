9fcdf4025f7b0706a805bd7153576d4e899a0afc
a-wiles <98373402+a-wiles@users.noreply.github.com>
2022-03-06 23:56:30 -0500

    Update js.js

--- Assets/js.js
+++ Assets/js.js
@@ -1,78 +1,144 @@
 // Data storage
 var score =0;
-var count = 90;
+var time = 60;
+var currentQuestions = 0;
+
+//Question Prompts
 var questions = [
     { 
-        prompt: "How many Data types are there in Javascript?.",
-        answers: {
-            a: "4",
-            b: "6",
-            c: "8",
-            d: "infinite"
-        },
-         correctAnswer: "c"
+        question: "How many Data Types are there in Javascript?",
+        answers: [
+            "4",
+            "6",
+            "8",
+            "Infinite"
+        ],
+         correctAnswer: "8"
     },
 
     { 
-        prompt: "What interaction can you do a user input?",
-        answers: {
-            a: "prompt",
-            b: "alert",
-            c: "confirm",
-            d: "you cannot do a user input in any interaction."
-        },
-         correctAnswer: "a"
+        question: "What interaction can you do a user input?",
+        answers: [
+            "prompt",
+            "alert",
+            "confirm",
+            "you cannot do a user input in any interaction."
+        ],
+         correctAnswer: "prompt"
     },
 
     { 
-        prompt: "What would you use to observe a user behavior?",
-        answers: {
-            a: "EventListener",
-            b: ".clickhere",
-            c: "button",
-            d: "event"
-        },
-         correctAnswer: "a"
+        question: "What would you use to observe a user behavior?",
+        answers: [
+            "EventListener",
+            ".clickhere",
+            "button",
+            "event"
+        ],
+         correctAnswer: "EventListener"
     },
 
     { 
-        prompt: "What do you use to call a function?",
-        answers: {
-            a: "Square Brackets",
-            b: "Quotes",
-            c: "Curly Brace",
-            d: "Parenthesis"
-        },
-         correctAnswer: "d"
+        question: "What do you use to call a function?",
+        answers: [
+            "Square Brackets",
+            "Quotes",
+            "Curly Brace",
+            "Parenthesis"
+        ],
+         correctAnswer: "Parenthesis"
     },
 ]
 
-var finalAnswers = [];
+//Element ID's from HTML
+var welcomeEl = document.getElementById("welcome");
+var questionsEl = document.getElementById("question");
+var optionsEl = document.getElementById("options");
+var buttonE1 = document.getElementById("start-button");
+
+//high Scores arrays
+var number_high_score = 3;
+var high_score = 'highScores';
+var highScoreString = localStorage.getItem(high_score);
+var highScores = JSON.parse(highScoreString) ?? [];
 
 
+//Start functions Event Listener
+document.getElementById("start-button").addEventListener("click", startQuiz);
 
-//Start functions
-document.getElementById("startButton").addEventListener("click", startQuiz);
+//For loop to cycle through all the questions.
+var promptOptions = function() {
+    optionsEl.innerHTML = "";
+    for (var i = 0; i < questions[currentQuestions].answers.length; i++) {
+        var selectedButton = document.createElement("button");
+            selectedButton.onclick = answerCheck;
+            selectedButton.textContent = questions[currentQuestions].answers[i];
+            optionsEl.appendChild(selectedButton);
+    }
+}
 
-function startQuiz () {
-    startTimer();
-};
+//Checking if the answer to your question is correct or incorrect
+function answerCheck () {
+    if (this.textContent == questions[currentQuestions].correctAnswer){
+        alert("Correct");
+        } else {
+        alert ("Incorrect, 20 seconds has been deducted from your time!");
+        time -= 20;
+        }
+        nextPrompt();
+}
 
-function getQuestions () {
+//Function to switch to the next question
+ function nextPrompt () {
+     currentQuestions++
+    if (currentQuestions >= questions.length) {
+        quizOver();
+    }
+    else {
+        questionsEl.textContent = questions[currentQuestions].question;
+        promptOptions();
+    }
+}
 
-};
+//If you run out of time or you answer all questions, triggers for local storage.
+function quizOver () {
+    alert("Your quiz is over!");
+    //saveHighScore();
+        localStorage.setItem(JSON.stringify(time));
+}
 
+
+// 60 second count down timer.
 function startTimer() {
-    var timeleft = 60;
+    time--;
 
     var pageTimer = setInterval(function (){
-    document.getElementById("countdown").textContent = timeleft + "seconds remaining";
+    document.getElementById("countdown").textContent = time + "seconds remaining";
 
-    timeleft -= 1;
-    if(timeleft <= 0){
+    time -= 1;
+    if(time <= -1){
         clearInterval(pageTimer);
+        quizOver ();
     }
     }, 1000);
-
-    console.log(countdown);
 };
+
+//function to actual begin entire quiz
+function startQuiz () {
+    startTimer();
+    welcomeEl.style.display = "none";
+    questionsEl.textContent = questions[currentQuestions].question;
+    optionsEl.textContent = "";
+    promptOptions ();
+};
+
+//high scores function
+
+//    function saveHighScore(score, highScores) {
+//      var initials = prompt('You got a highscore! Enter initials:');
+//      var newScore = { score, initials };
+//          highScores.push(newScore);
+//          highScores.sort((a, b) => b.score - a.score);
+//          highScores.splice(number_high_score);
+//          localStorage.setItem(high_score, JSON.stringify(highScores));
+//    };
