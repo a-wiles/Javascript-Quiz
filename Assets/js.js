7c950ca030c11d2c0f3560f8f2aa6ca8a9efdecb
Alex Wiles <alexandra.wiles6@gmail.com>
2022-03-06 23:22:50 -0500

    Finished basic game functions, still need css

--- Assets/js.js
+++ Assets/js.js
@@ -1,50 +1,127 @@
+// Data storage
 var score =0;
+var time = 60;
+var currentQuestions = 0;
+
+//Question Prompts
 var questions = [
     { 
-        prompt: "How many Data types are there in Javascript?.",
-        answers: {
-            a: "4",
-            b: "6",
-            c: "8",
-            d: "infinite"
-        },
-         correctAnswer: "c"
+        question: "How many Data Types are there in Javascript?",
+        answers: [
+            "4",
+            "6",
+            "8",
+            "Infinite"
+        ],
+         correctAnswer: "8"
     },
 
     { 
-        prompt: "What interaction can you do a user input?",
-        answers: {
-            a: "prompt",
-            b: "alert",
-            c: "confirm",
-            d: "you cannot do a user input in any interaction."
-        },
-         correctAnswer: "a"
+        question: "What interaction can you do a user input?",
+        answers: [
+            "prompt",
+            "alert",
+            "confirm",
+            "you cannot do a user input in any interaction."
+        ],
+         correctAnswer: "prompt"
     },
 
     { 
-        prompt: "What would you use to observe a user behavior?",
-        answers: {
-            a: "EventListener",
-            b: ".clickhere",
-            c: "button",
-            d: "event"
-        },
-         correctAnswer: "a"
+        question: "What would you use to observe a user behavior?",
+        answers: [
+            "EventListener",
+            ".clickhere",
+            "button",
+            "event"
+        ],
+         correctAnswer: "EventListener"
     },
 
     { 
-        prompt: "What do you use to call a function?",
-        answers: {
-            a: "Square Brackets",
-            b: "Quotes",
-            c: "Curly Brace",
-            d: "Parenthesis"
-        },
-         correctAnswer: "d"
+        question: "What do you use to call a function?",
+        answers: [
+            "Square Brackets",
+            "Quotes",
+            "Curly Brace",
+            "Parenthesis"
+        ],
+         correctAnswer: "Parenthesis"
     },
 ]
 
-for (i=0, i < questions.length, i++) {
+//Element ID's from HTML
+var welcomeEl = document.getElementById("welcome");
+var questionsEl = document.getElementById("question");
+var optionsEl = document.getElementById("options");
+var buttonE1 = document.getElementById("start-button");
 
+
+//Start functions Event Listener
+document.getElementById("start-button").addEventListener("click", startQuiz);
+
+//For loop to cycle through all the questions.
+var promptOptions = function() {
+    optionsEl.innerHTML = "";
+    for (var i = 0; i < questions[currentQuestions].answers.length; i++) {
+        var selectedButton = document.createElement("button");
+            selectedButton.onclick = answerCheck;
+            selectedButton.textContent = questions[currentQuestions].answers[i];
+            optionsEl.appendChild(selectedButton);
+    }
 }
+
+//Checking if the answer to your question is correct or incorrect
+function answerCheck () {
+    if (this.textContent == questions[currentQuestions].correctAnswer){
+        alert("Correct");
+        } else {
+        alert ("Incorrect, 20 seconds has been deducted from your time!");
+        time -= 20;
+        }
+        nextPrompt();
+}
+
+//Function to switch to the next question
+ function nextPrompt () {
+     currentQuestions++
+    if (currentQuestions >= questions.length) {
+        quizOver();
+    }
+    else {
+        questionsEl.textContent = questions[currentQuestions].question;
+        promptOptions();
+    }
+}
+
+//If you run out of time or you answer all questions, triggers for local storage.
+function quizOver () {
+    alert("Your quiz is over!");
+    window.prompt("Enter Intials Here for high score!")
+    localStorage.setItem(time);
+}
+
+
+// 60 second count down timer.
+function startTimer() {
+    time--;
+
+    var pageTimer = setInterval(function (){
+    document.getElementById("countdown").textContent = time + "seconds remaining";
+
+    time -= 1;
+    if(time <= -1){
+        clearInterval(pageTimer);
+        quizOver ();
+    }
+    }, 1000);
+};
+
+//function to actual begin entire quiz
+function startQuiz () {
+    startTimer();
+    welcomeEl.style.display = "none";
+    questionsEl.textContent = questions[currentQuestions].question;
+    optionsEl.textContent = "";
+    promptOptions ();
+};
